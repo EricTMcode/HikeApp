2a481484c957a40c76de0631d789775257b29dd4
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-13 13:33:47 +0200

    - Add a custom button style for hiking

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,15 +12,49 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width: 256, height: 256)
+            VStack {
+                //MARK: - HEADER
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        
+                        Spacer()
+                        
+                        Button {
+                            print("BUTTON PRESSED")
+                        } label: {
+                            CustomButtonView()
+                        }
+                        
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                    
+                } //: HEADER
+                .padding(.horizontal, 30)
+                
+                //MARK: - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                //MARK: - FOOTER
+                
             }
         }
         .frame(width: 320, height: 570)
