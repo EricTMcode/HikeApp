07584e99d8b28255f6ceaaaad3e2a79638a308e0
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-16 13:15:37 +0200

    - Create a buttonStyle

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -55,6 +55,24 @@ struct CardView: View {
                 
                 //MARK: - FOOTER
                 
+                Button {
+                    print("Button Pressed")
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(colors: [.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .padding(.vertical)
+                .padding(.horizontal,30)
+                .background(
+                    LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+                )
+                
+                .cornerRadius(40)
             }
         }
         .frame(width: 320, height: 570)
