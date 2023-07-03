75b048bf768e4efddf0c4abf433faed7e130ad4f
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-03 14:07:37 +0200

    - Add presentation detents

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -51,6 +51,8 @@ struct CardView: View {
                         }
                         .sheet(isPresented: $isShowingSheet) {
                             SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         }
                     }
                     Text("Fun and enjoyable outdoor activity for friends and families.")
--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -10,28 +10,46 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         List {
-            
-            HStack {
-                Spacer()
-                Image(systemName: "laurel.leading")
-                    .font(.system(size: 80, weight: .black))
-                
-                VStack(spacing: -10) {
-                    Text("Hike")
-                        .font(.system(size: 66, weight: .black))
+            Section {
+                HStack {
+                    Spacer()
+                    Image(systemName: "laurel.leading")
+                        .font(.system(size: 80, weight: .black))
                     
-                    Text("Editors' Choice")
-                        .fontWeight(.medium)
+                    VStack(spacing: -10) {
+                        Text("Hike")
+                            .font(.system(size: 66, weight: .black))
+                        
+                        Text("Editors' Choice")
+                            .fontWeight(.medium)
+                    }
+                    
+                    Image(systemName: "laurel.trailing")
+                        .font(.system(size: 80, weight: .black))
+                    Spacer()
                 }
-                
-                Image(systemName: "laurel.trailing")
-                    .font(.system(size: 80, weight: .black))
-                Spacer()
+                .foregroundStyle(
+                    LinearGradient(colors: [.customGreenLight, .customGreenMedium, .customGreenDark], startPoint: .top, endPoint: .bottom)
+                )
+                .padding(.top,8)
+                VStack(spacing: 8) {
+                    Text("Where can you find \nperfect tracks?")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                    
+                    Text("The hike which looks gorgeous in photos but is even better one your are actually there. The hike that you hope to do again someday. \nFind the best day hikes in the app.")
+                        .font(.footnote)
+                        .italic()
+                    
+                    Text("Dust off the boots! It's time for a walk.")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.customGreenMedium)
+                }
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 16)
+                .frame(maxWidth: .infinity)
             }
-            .foregroundStyle(
-                LinearGradient(colors: [.customGreenLight, .customGreenMedium, .customGreenDark], startPoint: .top, endPoint: .bottom)
-            )
-            .padding(.top,8)
+            .listRowSeparator(.hidden)
         }
     }
 }
